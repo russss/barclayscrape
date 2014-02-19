70420b11ceab1abeeb8e486a721e5a8561e1fb90
James Wheare <james@wheare.org>
2014-02-19 14:37:38 +0000

    Export to CSV script

---
+++ export_to_csv.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+require './barclayscrape'
+# require 'logger'
+
+SURNAME = ''
+MEMBERSHIP_NUMBER = ''
+ACCOUNTS = {
+    'gbp' => '',
+    'eur' => '',
+    'usd' => '',
+}
+
+print 'Identify with mobile pin sentry: '
+bs = BarclayScrape.new(
+    SURNAME,
+    MEMBERSHIP_NUMBER,
+    :motp => gets.strip,
+    # :logger => Logger.new(STDOUT)
+)
+
+ACCOUNTS.each do |label, number|
+    data = bs.extract_html_statement(number)
+    printf("Importing %s account: %s (%s rows)\n", label, number, data.length)
+    if data.length
+        File.open('export/' + label + '.csv', 'w') do |file|
+            data.each do |k, d|
+                ref = d['ref']
+                if d['ref2']
+                    ref = sprintf("%s-%s", ref, d['ref2'])
+                end
+                line = sprintf("%s,%s-%s-%s,%s\n", d['date'], d['trans-type'], d['description'], ref, d['amount'].gsub(/[£,]/, ''))
+                file.write(line)
+            end
+            file.close()
+        end
+    end
+end
