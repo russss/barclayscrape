9bde94f9185ab1b9a24a833855556bf41f0822d1
James Wheare <james@wheare.org>
2014-12-08 20:19:32 +0000

    Bring back export_csv html statement parsing and move more abstraction to barclayscrape lib

---
+++ export_csv.js
@@ -0,0 +1,83 @@
+#!/usr/bin/env casperjs
+var casper = require('casper').create({
+    verbose: true,
+    logLevel: "debug",
+    pageSettings: {
+        webSecurityEnabled: false
+    }
+});
+var barclayscrape = require("./barclayscrape");
+casper.start();
+
+function parseHtml(accountName, accountNumber) {
+    var page = 'https://bank.barclays.co.uk/olb/balances/SeeStatementsDirect.action?productIdentifier=' + accountNumber;
+    casper.thenOpen(page, function openAccount() {
+        // this.echo('Opened statement for: ' + accountNumber, "DEBUG");
+        this.then(function openAccountThen () {
+            var res = this.evaluate(function parseStatement() {
+                var txns = {};
+                var txn_list = [];
+                var rows = document.querySelectorAll('table#filterable tbody tr');
+                [].forEach.call(rows, function (row) {
+                    if (row.id) {
+                        var row_id = row.id.replace(/transaction_(\d+).*/, '$1');
+                        var txd;
+                        if (txns[row_id]) {
+                            txd = txns[row_id];
+                        } else {
+                            txd = {};
+                            txn_list.push(txd);
+                            txns[row_id] = txd;
+                        }
+                        if (row.id.indexOf('reveal') !== -1) {
+                            txd['amount'] = row.querySelector('.spend').innerText.trim();
+                            txd['trans-type'] = row.querySelector('.trans-type').innerText.trim();
+                            if (row.querySelectorAll('.keyword-search')[2]) {
+                                txd['ref'] = row.querySelectorAll('.keyword-search')[2].innerText.trim();
+                            }
+                            if (row.querySelectorAll('.keyword-search')[3]) {
+                                txd['ref2'] = row.querySelectorAll('.keyword-search')[3].innerText.trim();
+                            }
+                        } else {
+                            txd['date'] = row.querySelector('.date').innerText.trim();
+                            txd['description'] = row.querySelector('.description').innerText.trim();
+                            txd['balance'] = row.querySelector('.balance').innerText.trim();
+                        }
+                    }
+                });
+                return txn_list;
+            });
+            var statement = [].slice.call(res);
+            writeCsv(accountName, accountNumber, statement);
+        });
+    });
+}
+
+function writeCsv(accountName, accountNumber, statement) {
+    var filename = accountName + '.csv';
+    var csvLines = statement.map(function (d) {
+        var ref = '';
+        if (d.ref) {
+            ref = "-" + d.ref;
+            if (d.ref2) {
+                ref = ref + '-' + d.ref2;
+            }
+        }
+        return d['date'] + ',' + d['trans-type'] + '-' + d['description'] + ref + ',' + d['amount'].replace(/[£,]/g, '');
+    });
+
+    require('fs').write('export/' + filename, [].join.call(csvLines, '\n'), 'w');
+    casper.echo("Exporting " + accountName + " account: " + accountNumber + " (" + csvLines.length + " rows)");
+}
+
+barclayscrape.login(casper, {
+    onAccounts: function (accounts) {
+        // Iterate through each account and export it
+        for (var accountName in accounts) {
+            var accountNumber = accounts[accountName];
+            parseHtml(accountName, accountNumber);
+        }
+    }
+});
+
+casper.run();
--- get_ofx.js
+++ get_ofx.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env casperjs
 var casper = require('casper').create({
     verbose: true,
     logLevel: "error",
@@ -5,77 +6,45 @@ var casper = require('casper').create({
         webSecurityEnabled: false
     }
 });
-var childProcess = require("child_process");
 var barclayscrape = require("./barclayscrape");
 casper.start();
-var accounts = [];
-if (!casper.cli.has("otp")) {
-    casper.die("Usage: casperjs ./get_ofx.js --otp=12345678");
+
+function downloadOfx (accountName, accountNumber) {
+    casper.thenOpen('https://bank.barclays.co.uk/olb/balances/ExportDataStep1.action', function() {
+        this.echo("Exporting account: " + accountNumber, "INFO");
+        this.fill('.process-form', {
+                 'reqSoftwarePkgCode': 7, // Microsoft Money 2002 (i.e. OFX)
+                 'productIdentifier': accountNumber});
+    });
+    casper.thenClick('input[type="submit"]'); // Next
+    casper.waitForSelector('#fromExportDate'); // Wait for the details form
+    casper.thenClick('.btn-right input[type="submit"]'); // Next
+    casper.waitForSelector('.body', function exportConfirmation() { // Wait for confirmation page
+        if (casper.exists('div.section-error')) {
+            this.echo("Can't download OFX for " + accountNumber + " (probably nothing there)", "COMMENT");
+        } else {
+            var filename = accountName + '.ofx';
+            this.echo("Downloading " + filename, "INFO");
+            // Here we have to be a bit messy and send the post request manually, as
+            // Casper/Phantom has no way of catching the downloaded file if we click the button.
+            var requestid = this.getElementAttribute('input[name="requestid"]', 'value');
+            this.download('https://bank.barclays.co.uk/olb/balances/ExportDataStep2.action', filename, 'POST', {
+                'requestid': requestid,
+                'requesttoken': '',
+                'action:ExportDataStep2All_download': 'Download'
+            });
+        }
+    });
 }
 
-casper.echo('Barclayscrape Starting', 'INFO');
-
-casper.on('error', function() {
-    this.capture('error.png');
-    this.exit(1);
+barclayscrape.login(casper, {
+    onAccounts: function (accounts) {
+        // Iterate through each account and export it
+        for (var accountName in accounts) {
+            var accountNumber = accounts[accountName];
+            downloadOfx(accountName, accountNumber);
+        }
+    }
 });
 
-casper.userAgent('Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)');
-
-barclayscrape.login(casper, String(casper.cli.get('otp')));
-
-// Load the "Export my Data" modal. There might be an export data widget on the dashboard,
-// but we shouldn't rely on it.
-casper.thenClick('a#accounts-menu');
-casper.waitForSelector('.mega-menu-wrapper'); // "Mega"
-casper.thenClick('a[href="/olb/balances/ExportDataStep1.action"]');
-
-// Obtain a list of all accounts
-casper.then(function fetchAccountList() {
-    accounts = this.evaluate(function() {
-        var nodes = document.querySelectorAll('div#listproductIdentifier input');
-        var account_nodes = [].filter.call(nodes, function(node) {
-            // Remove "special" account names (all/business/personal)
-            return node.value != 'All' && node.value != 'BUSINESS' && node.value != 'PERSONAL';
-        });
-        return [].map.call(account_nodes, function(node) {
-            return node.value;
-        });
-    });
-
-    // Iterate through each account and export it
-    this.each(accounts, function fetchAccount(self, account) {
-        self.thenOpen('https://bank.barclays.co.uk/olb/balances/ExportDataStep1.action', function() {
-            this.echo("Exporting account: " + account, "INFO");
-            this.fill('.process-form', {
-                     'reqSoftwarePkgCode': 7, // Microsoft Money 2002 (i.e. OFX)
-                     'productIdentifier': account});
-        });
-        self.thenClick('input[type="submit"]'); // Next
-        self.waitForSelector('#fromExportDate'); // Wait for the details form
-        self.thenClick('.btn-right input[type="submit"]'); // Next
-        self.waitForSelector('.body', function exportConfirmation() { // Wait for confirmation page
-            if (casper.exists('div.section-error')) {
-                this.echo("Can't download OFX for " + account + " (probably nothing there)", "COMMENT");
-            } else {
-                var filename = account + '.ofx';
-                this.echo("Downloading " + filename, "INFO");
-                // Here we have to be a bit messy and send the post request manually, as
-                // Casper/Phantom has no way of catching the downloaded file if we click the button.
-                var requestid = this.getElementAttribute('input[name="requestid"]', 'value');
-                this.download('https://bank.barclays.co.uk/olb/balances/ExportDataStep2.action',
-                              filename, 'POST',
-                              {'requestid': requestid,
-                               'requesttoken': '',
-                               'action:ExportDataStep2All_download': 'Download'
-                              });
-            }
-        });
-    });
-});
-
-
-casper.run(function complete() {
-    this.echo("Accounts exported: " + accounts, "INFO");
-    this.exit();
-});
+casper.run();
