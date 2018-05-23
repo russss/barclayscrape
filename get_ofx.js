654b844aefed26f0726dc25da71b9d34ec29adbe
Russ Garrett <russ@garrett.co.uk>
2018-05-23 08:31:14 +0000

    Fix OFX downloading issue

--- get_ofx.js
+++ get_ofx.js
@@ -6,6 +6,7 @@ var casper = require('casper').create({
         webSecurityEnabled: false
     }
 });
+var x = require('casper').selectXPath;
 var barclayscrape = require("./barclayscrape");
 
 casper.start();
@@ -41,19 +42,14 @@ function downloadOfx (account) {
 }
 
 function doDownload(account) {
-    casper.thenClick("a.export");
-    casper.waitForSelector("div.export-options");
-    casper.then(function downloadOFXFile() {
-        // Here we cheat and just fetch the URL.
-        var filename = 'export/' + account.name + '.ofx';
-        var extraLog = '';
-        if (account.name != account.number) {
-            extraLog = ' (' + account.number + ')'
-        }
-        this.echo("Downloading " + account.name + extraLog);
-        var url = 'https://bank.barclays.co.uk/olb/balances/ExportData_FTB.action?reqSoftwarePkgCode=4&accountIdentifierIndex=' + account.idx;
-        this.download(url, filename);
-    });
+    var filename = 'export/' + account.name + '.ofx';
+    var extraLog = '';
+    if (account.name != account.number) {
+        extraLog = ' (' + account.number + ')'
+    }
+    casper.echo("Downloading " + account.name + extraLog);
+    var el = casper.getElementInfo(x("//a[text()[contains(., ' Money 2001')]]"));
+    casper.download(el['attributes']['href'], filename);
 }
 
 barclayscrape.login(casper, {
